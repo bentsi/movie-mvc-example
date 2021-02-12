eba99233cc5af62110f51f8f0a2b507731516326
Aleksei <45937103+AleksejMoiseev@users.noreply.github.com>
2021-02-12 13:54:16 +0300

    feat(view.py): Написан view mvc

--- movie/view.py
+++ movie/view.py
@@ -1,17 +1,25 @@
-from movie.controller import Controller
-from movie.model import Model
+from movie import Actor, Movie
+from movie.controller import Conector
 
 
 class View:
 
     def __init__(self):
-        self.controller = Controller()
-        self.model = Model()
+        self.conector = Conector()
+
+    def add_movie(self, movie_obj):
+        self.conector.add_movie(movie_obj)
 
     def show_all(self):
-        movies = self.model.get_all_movies()
+        movies = self.conector.show_all()
         for movie in movies:
             print(movie)
 
-    def add_movie(self, movie_obj):
-        self.controller.add_movie(movie_obj)
+    def dump(self):
+        self.conector.dump()
+
+
+if __name__ == '__main__':
+    view = View()
+    peter = [Actor(name="Peter", last_name="First", role="main")]
+    movie_obj = Movie(title="Firs Peter", date_realized=2020, actor=peter)
