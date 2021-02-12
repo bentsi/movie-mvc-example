cdde6795759d0d1acb0689496ee9fa88b227f606
Aleksei <45937103+AleksejMoiseev@users.noreply.github.com>
2021-02-12 13:50:49 +0300

    feat(controller.py): Написан контроллер mvc

--- movie/controller.py
+++ movie/controller.py
@@ -1,9 +1,15 @@
 from movie.model import Model
 
 
-class Controller:
+class Conector:
     def __init__(self):
         self.model = Model()
 
     def add_movie(self, movie_obj):
         self.model.add_movie(movie_obj)
+
+    def show_all(self):
+        return self.model.get_all_movie()
+
+    def dump(self):
+        self.model.dump_to_yml()
