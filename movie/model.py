6fca8a029b7c7e43b4b4a802f5cdeb23d7f68c51
Aleksei <45937103+AleksejMoiseev@users.noreply.github.com>
2021-02-12 13:48:19 +0300

    feat(model.py): Написана модель mvc

--- movie/model.py
+++ movie/model.py
@@ -1,19 +1,58 @@
+import yaml
+
+from movie import Movie, Actor
+
+
 class Model:
+
     def __init__(self):
         self._dump_filename = "./movies.dump"
-        self._all_movies = []
+        self._list_movie = []
         self.load()
 
-    def get_all_movies(self):
-        return self._all_movies
+    def add_movie(self, movie_obj):
+        self._list_movie.append(movie_obj)
+        self.show_all()
 
-    def dump(self, movie_obj):
-        pass
+    def dump_to_yml(self):
+        list_for_dump = []
+        with open(self._dump_filename, "at") as file:
+            for line in self._list_movie:
+                list_for_dump.append(line.dict_for_to_yaml())
+            yaml.dump(list_for_dump, file)
+        return list_for_dump
 
     def load(self):
-        """loads all movies from file"""
-        pass
+        """ load all movie"""
+        try:
+            with open(self._dump_filename, "rt") as file:
+                str_obj = yaml.safe_load(file)
+                for mov in str_obj:
+                    self._list_movie.append(Movie.make_obj(dict_serilization=mov))
+                return self._list_movie
+        except (FileNotFoundError, TypeError):
+            f = open(self._dump_filename, "wt")
+            f.close()
 
-    def add_movie(self, movie_obj):
-        self._all_movies.append(movie_obj)
-        self.dump(movie_obj)
+    def get_all_movie(self):
+        if not self._list_movie:
+            return ("Список пуст", )
+        return self._list_movie
+
+    def show_all(self):
+        movies = self.get_all_movie()
+        for movie in movies:
+            print(movie)
+        return movies
+
+
+if __name__ == '__main__':
+    mod = Model()
+    actors = [
+        Actor(name="Peter", last_name="First", role="main_Hero"),
+        Actor(name="Kira", last_name="Knightley", role="Joan Clarke"),
+        Actor(name="Benedict", last_name="Cumberbatch", role="Alan Turing")
+    ]
+    movie_obj = Movie(title="Firs Peter", date_realized=2020, actor=actors)
+    mod.add_movie(movie_obj=movie_obj)
+    mod.show_all()
