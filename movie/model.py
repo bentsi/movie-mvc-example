9091e2e64342c38362b0681fef54aeafd9f5519d
Komachi Onozuka <onozuka.komachi@outlook.com>
2021-02-15 18:48:49 +0300

    feat(movie): Basic implementation

--- movie/model.py
+++ movie/model.py
@@ -1,19 +1,30 @@
+import json
+from movie import Actor, Movie
+
 class Model:
     def __init__(self):
-        self._dump_filename = "./movies.dump"
+        self._dump_filename = "./movies.json"
         self._all_movies = []
-        self.load()
+        self.load_all()
 
     def get_all_movies(self):
         return self._all_movies
 
-    def dump(self, movie_obj):
-        pass
+    def dump_all(self):
+        with open(self._dump_filename, "wt") as f:
+            movie_dicts = [dict(movie.__dict__) for movie in self._all_movies]
+            for movie_dict in movie_dicts:
+                movie_dict["actors"] = [actor.__dict__ for actor in movie_dict["actors"]]
+            json.dump(movie_dicts, f)
 
-    def load(self):
-        """loads all movies from file"""
-        pass
+    def load_all(self):
+        with open(self._dump_filename, "rt") as f:
+            movies_serialized = json.load(f)
+            for movie_dict in movies_serialized:
+                actors = [Actor(**actor) for actor in movie_dict["actors"]]
+                movie = Movie(movie_dict["title"], movie_dict["release_date"], actors)
+                self._all_movies.append(movie)
 
     def add_movie(self, movie_obj):
         self._all_movies.append(movie_obj)
-        self.dump(movie_obj)
+        self.dump_all()
