81e66c6ce6e2b6572bbf620850d183c693e681d1
Yakov Gurev <onlygituse@gmail.com>
2021-03-04 14:27:10 +0300

    fix: all functions work
    Yeee

--- movie/controller.py
+++ movie/controller.py
@@ -1,9 +1,6 @@
-from movie.model import Model
-
-
 class Controller:
-    def __init__(self):
-        self.model = Model()
+    def __init__(self, model):
+        self.model = model
 
     def add_movie(self, movie_obj):
         self.model.add_movie(movie_obj)
--- movie/model.py
+++ movie/model.py
@@ -1,18 +1,30 @@
+import pickle
+
+
 class Model:
     def __init__(self):
         self._dump_filename = "./movies.dump"
         self._all_movies = []
-        self.load()
+        self._all_file_name = []
 
     def get_all_movies(self):
         return self._all_movies
 
     def dump(self, movie_obj):
-        pass
+        file_name = f"movies/{movie_obj.title}_{movie_obj.date_released}.pickle"
+        file = open(file_name, "wb")
+        pickle.dump(movie_obj, file)
+        file.close()
+        self._all_file_name.append(file_name)
 
     def load(self):
-        """loads all movies from file"""
-        pass
+        all_load_movies = []
+        for name in self._all_file_name:
+            file = open(name, "rb")
+            movie_from_file = pickle.load(file)
+            file.close()
+            all_load_movies.append(movie_from_file)
+        return all_load_movies
 
     def add_movie(self, movie_obj):
         self._all_movies.append(movie_obj)
